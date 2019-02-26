1666c6d9d25ff475e56da24908c93cd2f89aab8c
Boris <bpzweber@gmail.com>
2019-02-26 16:19:33 +0100

    A Project Requires An Owner Test

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -17,9 +17,21 @@ class ProjectsTest extends TestCase
      */
 
     /** @test */
+    public function only_authenticated_users_can_create_projects() {
+
+        /*$this->withoutExceptionHandling();*/
+
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
     public function a_user_can_create_project()
     {
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -48,6 +60,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title() {
 
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -56,8 +70,11 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description() {
 
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
 }
