96e81406e7349e969347c598d3b1bab9f65d0e1b
Boris <bpzweber@gmail.com>
2019-03-06 14:14:14 +0100

    Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,19 +6,18 @@
         <a href="/projects/create">New Project</a>
     </div>
 
-
-    <ul>
+    <div class="flex">
         @forelse($projects as $project)
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height:200px">
+            <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
 
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-
+            <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+        </div>
         @empty
 
-            <p>No projects yet</p>
+            <div>No projects yet</div>
 
         @endforelse
-    </ul>
+    </div>
 
 @endsection
