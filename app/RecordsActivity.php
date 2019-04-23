07702596db95b7f6fc62270084bafa00cc9e8f40
Boris <bpzweber@gmail.com>
2019-04-23 15:00:33 +0200

    Tweaking the Activity Card

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -10,36 +10,35 @@ trait RecordsActivity
      * @var array
      */
     public $oldAttributes = [];
-
     /**
      * Boot the trait.
      */
     public static function bootRecordsActivity()
     {
-
         foreach (self::recordableEvents() as $event) {
             static::$event(function ($model) use ($event) {
                 $model->recordActivity($model->activityDescription($event));
             });
-
-            if($event === 'updated') {
+            if ($event === 'updated') {
                 static::updating(function ($model) {
                     $model->oldAttributes = $model->getOriginal();
                 });
             }
         }
     }
-
     /**
-     * @param $description
+     * Get the description of the activity.
+     *
+     * @param  string $description
      * @return string
      */
     protected function activityDescription($description)
     {
         return "{$description}_" . strtolower(class_basename($this));
     }
-
     /**
+     * Fetch the model events that should trigger activity.
+     *
      * @return array
      */
     protected static function recordableEvents()
@@ -47,11 +46,8 @@ trait RecordsActivity
         if (isset(static::$recordableEvents)) {
             return static::$recordableEvents;
         }
-
         return ['created', 'updated', 'deleted'];
-
     }
-
     /**
      * Record activity for a project.
      *
@@ -60,12 +56,12 @@ trait RecordsActivity
     public function recordActivity($description)
     {
         $this->activity()->create([
+            'user_id' => ($this->project ?? $this)->owner->id,
             'description' => $description,
             'changes' => $this->activityChanges(),
             'project_id' => class_basename($this) === 'Project' ? $this->id : $this->project_id
         ]);
     }
-
     /**
      * The activity feed for the project.
      *
@@ -73,9 +69,11 @@ trait RecordsActivity
      */
     public function activity()
     {
+        if (get_class($this) === Project::class) {
+            return $this->hasMany(Activity::class)->latest();
+        }
         return $this->morphMany(Activity::class, 'subject')->latest();
     }
-
     /**
      * Fetch the changes to the model.
      *
@@ -85,8 +83,12 @@ trait RecordsActivity
     {
         if ($this->wasChanged()) {
             return [
-                'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
-                'after' => array_except($this->getChanges(), 'updated_at')
+                'before' => array_except(
+                    array_diff($this->oldAttributes, $this->getAttributes()), 'updated_at'
+                ),
+                'after' => array_except(
+                    $this->getChanges(), 'updated_at'
+                )
             ];
         }
     }
